6735ceb06a1a7f8cf603011ce367779d242fc66c
Zaki Ali <zihali91@gmail.com>
2018-11-09 17:03:46 +0000

    completed tutorial for week5

--- week5/week5exercises/ArrayIII.java
+++ week5/week5exercises/ArrayIII.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
 public class ArrayIII{
 	public static void main(String[] args){
-
-		Scanner input = new Scanner(System.in);
-		int N = Integer.parseInt(args[0]);
+		int N = args.length;
 		int [] a = new int [N]; //declared and creted array with size N
+		boolean result = true;
+		for (int i = 0; i < N ; i++){
+            //initialised array
+  			a[i] = Integer.parseInt(args[i]);
+  		}
+			for (int i = 0; i < N ; i++){
+                for(int j = i + 1; j < N; j++){
+                	if (a[i] == a[j])
+                	result = false;
+                }
+            }
+            System.out.println(result);    
 
-		for (int i = 0; i < N -1 ; i++){ //initialised array 
-			a[i] = input.nextInt();
-		}
 	}
 }
 
